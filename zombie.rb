85d2fce9fea67d0118b18e35abb7538925c2f57b
Brian Du <brian.bh.du@gmail.com>
2017-04-05 19:53:11 -0400

    Part 3 - Zombie Apocalypse 1.1
    - Completed assignment

--- zombie.rb
+++ zombie.rb
@@ -18,16 +18,22 @@ class Zombie
     @strength = strength
   end
 
-  def encounter
-
+  def outrun_zombie? # completed
+    zombie = @@horde[ rand( @@horde.count + 1 ) ]
+    if rand( @@max_speed + 1 ) >= zombie.speed
+      return true
+    else
+      return false
+    end
   end
 
-  def outrun_zombie?
-
-  end
-
-  def survive_attack?
-
+  def survive_attack? # completed
+    zombie = @@horde[ rand( @@horde.count + 1 ) ]
+    if rand( @@max_strength + 1 ) >= zombie.strength
+      return true
+    else
+      return false
+    end
   end
 
 # CLASS METHODS
@@ -37,25 +43,39 @@ class Zombie
   end
 
   def self.spawn # complete
-    spawn_quantity = @@plague_level / 5
+    spawn_quantity = @@plague_level / 2
     spawn_quantity.times do
-      zombie = Zombie.new(rand(6),rand(9))
+      zombie = Zombie.new(rand(@@max_speed + 1),rand(@@max_strength + 1))
       @@horde.push(zombie)
     end
   end
 
-  def self.new_day
-
+  def self.new_day # complete
+    some_die_off
+    spawn
+    increase_plague_level
+    puts "#{some_die_off} zombies died and #{spawn} zombies spawned today. Plague level also increased to #{increase_plague_level}."
   end
 
-  def self.some_die_off
-
+  def self.some_die_off # complete
+    number_remove = rand(11)
+    number_remove.times do
+      @@horde.delete_at( rand( @@horde.count + 1 ) )
+    end
   end
 
   def self.increase_plague_level # complete
     @@plague_level = @@plague_level + rand(3)
   end
 
-
+  def self.encounter # complete
+    encounter = outrun_zombie? || survive_attack?
+    if encounter == true
+      puts "You escaped!"
+    else
+      puts "You are now infected. You became a zombie."
+      abort
+    end
+  end
 
 end
